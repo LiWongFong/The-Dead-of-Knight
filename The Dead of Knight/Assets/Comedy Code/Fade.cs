6ebae960cd1ef6e3e6eff2547b3de735ee911487
LiWongFong <123572550+LiWongFong@users.noreply.github.com>
2023-02-10 17:24:21 -0600

    Fixed the script errors

---
+++ The Dead of Knight/Assets/Comedy Code/Fade.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = transform.GetComponent<SpriteRenderer>().material.color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+            transform.GetComponent<SpriteRenderer>().material.color = newColor;
+            yield return null;
+        }
+    }
+}
