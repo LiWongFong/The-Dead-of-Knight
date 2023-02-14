588b0a2496b1da3ccfed04375de7293b77715ee1
LiWongFong <123572550+LiWongFong@users.noreply.github.com>
2023-02-14 15:40:30 -0600

    The hell of Shaders has been unleashed

---
+++ The Dead of Knight/Assets/Comedy Code/Fixy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fixy : MonoBehaviour
+{
+    void Start()
+    {
+        TransManager.tManager.Load("W10");
+    }
+}
