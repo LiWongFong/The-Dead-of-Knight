24fc86d15047a84f34fe143fe875a201205412d8
LiWongFong <123572550+LiWongFong@users.noreply.github.com>
2023-05-02 11:27:23 -0500

    Added new animations

--- The Dead of Knight/Assets/Comedy Code/CustomButton.cs
+++ The Dead of Knight/Assets/Comedy Code/CustomButton.cs
@@ -9,5 +9,21 @@ using UnityEngine.UI;
 [AddComponentMenu("UI/Custom Button", 31)]
 public class CustomButton : Button
 {
+    public void textUpdate()
+    {
+        switch (currentSelectionState)
+        {
+            case SelectionState.Pressed:
+                //move text down 12.6
+                break;
+            default:
+                break;
+        }
+    }
 
+    [ContextMenu("hfgkjhdbfgkhdxbgi")]
+    public void test()
+    {
+        print(currentSelectionState);
+    }
 }
--- The Dead of Knight/Assets/Comedy Code/Invisible.cs
+++ The Dead of Knight/Assets/Comedy Code/Invisible.cs
@@ -6,11 +6,11 @@ public class Invisible : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        
+        GetComponent<SpriteRenderer>().enabled = true;
     }
 }
